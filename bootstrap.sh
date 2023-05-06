eb86aa276101e0739c1880075315568afbf4765c
agryphus <agryphus@gmail.com>
2023-05-05 23:21:44 -0400

    Resolved git ssh cloning issue

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,19 +3,31 @@
 # Simple script to setup my Fedora environment the way I like it.
 # Version: 36 (Container Image)
 
+# Download passwd first.
+echo "Installing passwd... Might take a second to update dnf."
+echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
+dnf install -y passwd
+
 # Create new user
 echo -n "Username: "
 read username 
 echo "Hello, $username"
 useradd $username 
 usermod -aG wheel $username # Grant sudo
-
-# Create password
-dnf install -y passwd
 passwd $username
 
 # Treat user's home as new home for rest of script
 export HOME=/home/$username
+
+# Git account info
+echo -n "Git username: "
+read git_username
+echo -n "Git email: "
+read git_email
+set_git=true
+if [ "$git_username" = "" ] || [ "$git_email" = "" ]; then
+    set_git=false
+fi
 
 # Copy over git private key
 echo -n "Git id_rsa location (skip if you don't have one): "
@@ -45,6 +57,12 @@ dnf install -y clang
 dnf install -y unzip
 dnf install -y git
 
+# Configure git
+if $set_git; then
+    git config --global user.name $git_username
+    git config --global user.email $git_email
+fi
+
 # Change shells
 chsh -s /usr/bin/zsh $username # ZSH as interactive shell
 ln -sf /bin/dash /bin/sh       # Dash as /bin/sh
@@ -53,10 +71,9 @@ ln -sf /bin/dash /bin/sh       # Dash as /bin/sh
 cd ~
 test -d ./.config || mkdir .config
 cd .config
+git clone https://github.com/agryphus/nvim.git
 if $use_ssh; then
-    git clone git@github.com:agryphus/nvim.git
-else
-    git clone https://github.com/agryphus/nvim.git
+    git remote set-url origin git@github.com:agryphus/nvim.git
 fi
 
 # Packer
@@ -66,10 +83,9 @@ nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
 # Copy over dotfiles
 cd ~/.config
+git clone https://github.com/agryphus/dotfiles.git
 if $use_ssh; then
-    git clone git@github.com:agryphus/dotfiles.git
-else
-    git clone https://github.com/agryphus/dotfiles.git
+    git remote set-url origin git@github.com:agryphus/dotfiles.git
 fi
 
 # Setup zsh
@@ -82,10 +98,10 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 # Editing WSL configuration
 touch /etc/wsl.conf
 echo "[interop]
-appendWindowsPat h= false
+appendWindowsPath=false
 
 [boot]
-systemd = true
+systemd=true
 " >> /etc/wsl.conf
 
 # Finish up
